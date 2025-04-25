1ff618477d0e34e019d5ebd7ed9e53622cf32810
Muhammad Yusuf Adhi Surya <yusup22002@mail.unpad.ac.id>
2025-04-25 13:31:22 +0700

    commit database challenge

---
+++ Enviro/database/migrations/0001_01_01_000009_create_challenge_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('challenge_reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('challenge_id');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('challenge_id')->references('id')->on('challenges')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('challenge_reports');
+    }
+};
